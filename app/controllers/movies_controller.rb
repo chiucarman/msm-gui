4ea2238ca6d93cc4c8a46cfaee19900736f79cb7
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-10-05 19:48:24 +0000

    adds movie working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,27 @@
 class MoviesController < ApplicationController
+  def create
+    # params hash looks like this: {"the_title"=>"1", "the_year"=>"3", "the_duration"=>"6", "the_description"=>"7", "the_image"=>"9", "the_director_id"=>"4"}
+
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+  
+    redirect_to("/movies")
+
+      # Retrieve the user's inputs from params
+      # Create a record in the movie table
+      # Populate each column with the user input
+      # Save
+
+      # Redirect the user back to the /movies URL
+  end
+  
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
