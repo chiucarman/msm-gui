e4116b1ea025c7f0c840f767ba90af47482fa5bb
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-10-06 15:57:51 +0000

    delete director link working

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -11,6 +11,18 @@ class DirectorsController < ApplicationController
     redirect_to("/directors")
   end
 
+  def destroy
+    the_id = params.fetch("the_id")
+
+    matching_records = Director.where({ :id => the_id })
+
+    the_director = matching_records.at(0)
+
+    the_director.destroy
+
+    redirect_to("/directors")
+  end
+
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
